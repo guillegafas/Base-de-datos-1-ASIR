2d538e5402fb8d3a8aeebdc238a0fa08c23b3a22
guillegafas <116709250+guillegafas@users.noreply.github.com>
2022-11-10 13:43:56 +0100

    Add files via upload

---
+++ workbench/concesionario-Guillermo-Muñoz.sql
@@ -0,0 +1,11 @@
+CREATE TABLE intermedia (
+Codcoche INT NOT NULL,
+Codreserva INT NOT NULL,
+Entrega INT NOT NULL,
+gasolina INT NOT NULL,
+PRIMARY KEY (Codcoche, Codreserva)
+) ENGINE InnoDB; 
+
+
+
+
